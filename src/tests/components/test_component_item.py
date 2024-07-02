38843cb863c308ef1e1d5550a0b73f8214516ac2
John Doe <johndoe@gmail.com>
2024-07-02 09:06:33 +0200

    don't need these

--- src/tests/components/test_component_item.py
+++ src/tests/components/test_component_item.py
@@ -4,7 +4,6 @@ from packages.verbs import *
 from base_obj import new_object
 from physical_obj import PhysObj
 from player import Player
-from packages.components.inventory import ComponentInventory
 from packages.components.item import ComponentItem
 
 
@@ -15,8 +14,6 @@ class TestClass2(TestClass):
         global_textadv.player_ref = new_object(Player)
         self.fresh_room.add_to_room(global_textadv.player_ref)
         item: PhysObj = new_object(PhysObj, "debug_item_2")
-        # inventory_component: ComponentInventory = global_textadv.player_ref.get_component(
-        #    ComponentInventory)
         item_component: ComponentItem = item.get_component(ComponentItem)
         self.fresh_room.add_to_room(item)
         item_component.unmoved_examine = "Unmoved examine text"
